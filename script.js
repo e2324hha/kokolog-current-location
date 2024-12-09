88a382b95bf30a866f872fc94fd1b821768ccf43
e2324hha <pompome2324@gmail.com>
2024-12-09 16:05:03 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    //地図作成
+    const map = L.map('map').setView([35.46885, 139.52191], 18)
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    const marker = L.marker([35.46885, 139.52191]).addTo(map);
+    var name="ふみか";
+    var date=new Date();
+    var datetime=date.getMonth()+"月"+date.getDate()+"日"+date.getHours()+':'+date.getMinutes();
+    marker.bindPopup(name + '<b>さんはここにいます</b><br>'+datetime).openPopup();
+})
