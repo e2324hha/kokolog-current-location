0faba7275b02854612ab5fb4ce05c9acc0bc1aad
e2324hha <pompome2324@gmail.com>
2025-02-06 13:40:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
     const marker = L.marker([35.46885, 139.52191]).addTo(map);
-    var name="ふみか";
+    var name="はるな";
     var date=new Date();
     var datetime=date.getMonth()+"月"+date.getDate()+"日"+date.getHours()+':'+date.getMinutes();
     marker.bindPopup(name + '<b>さんはここにいます</b><br>'+datetime).openPopup();
